c00ea86ed6d78627c9b553492b8a4a4e9c73879b
wojiushixiaobai <296015668@qq.com>
2020-03-24 18:33:27 +0800

    [Update]更新升级流程

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -11,8 +11,8 @@ if [ $Version == $Upgrade_Version ]; then
     exit 0
 fi
 
-echo -e "\033[31m 准备升级到 $Upgrade_Version ... \033[0m"
-jumpserver_backup=${PROJECT_DIR}/backup/$(date -d "today" +"%Y%m%d_%H%M%S")
+echo -e "\033[31m 准备从 $Version 升级到 $Upgrade_Version ... \033[0m"
+jumpserver_backup=${PROJECT_DIR}/backup/$Version
 if [ ! -d "$jumpserver_backup" ]; then
     mkdir -p $jumpserver_backup
 fi
@@ -22,16 +22,27 @@ docker rm jms_koko jms_guacamole
 docker rmi wojiushixiaobai/jms_koko:$Version wojiushixiaobai/jms_guacamole:$Version
 systemctl stop jms_core
 
-cp $install_dir/jumpserver $jumpserver_backup/jumpserver_$Version -r
-mysqldump -h$DB_HOST -P$DB_PORT -u$DB_USER -p$DB_PASSWORD $DB_NAME > $jumpserver_backup/$DB_NAME.sql
+if [ ! -d "$jumpserver_backup/jumpserver" ]; then
+    cp $install_dir/jumpserver $jumpserver_backup/ -r
+fi
+if [ ! -f "$jumpserver_backup/$DB_NAME.sql" ]; then
+    mysqldump -h$DB_HOST -P$DB_PORT -u$DB_USER -p$DB_PASSWORD $DB_NAME > $jumpserver_backup/$DB_NAME.sql
+fi
+
 echo -e "\033[31m >>> 已备份文件到 $jumpserver_backup <<< \033[0m"
 
 cd $install_dir/jumpserver
-git pull
+git pull || {
+    echo "获取 jumpserver 仓库更新失败"
+    exit 1
+}
 
 source $install_dir/py3/bin/activate
 pip install --upgrade pip setuptools
-pip install -r $install_dir/jumpserver/requirements/requirements.txt
+pip install -r $install_dir/jumpserver/requirements/requirements.txt || {
+    echo "升级 python 依赖失败"
+    exit 1
+}
 
 systemctl start jms_core
 
@@ -39,4 +50,5 @@ docker run --name jms_koko -d -p $ssh_port:2222 -p 127.0.0.1:5000:5000 -e CORE_H
 
 docker run --name jms_guacamole -d -p 127.0.0.1:8081:8080 -e JUMPSERVER_SERVER=http://$Server_IP:8080 -e BOOTSTRAP_TOKEN=$BOOTSTRAP_TOKEN --restart=always wojiushixiaobai/jms_guacamole:$Upgrade_Version
 
+sed -i "s/Version=$Version/Version=$Upgrade_Version/g" ${PROJECT_DIR}/config.conf
 echo -e "\033[31m >>> 已升级版本至 $Upgrade_Version <<< \033[0m"
