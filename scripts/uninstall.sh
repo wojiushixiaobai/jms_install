65c4376b98a9b777ddfc07b340bd6875397aa097
Administrator <admin@example.com>
2020-04-02 14:44:55 +0800

    [Fix]修改卸载文档

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -7,19 +7,19 @@ source ${PROJECT_DIR}/config.conf
 
 echo -e "\033[31m 准备从系统中卸载 jumpserver \033[0m"
 
-if [ ! "$(systemctl status nginx | grep running)" ]; then
+if [ "$(systemctl status nginx | grep running)" ]; then
     systemctl stop nginx
 fi
 rm -rf /etc/nginx/conf.d/jumpserver.conf
 
-if [ ! "$(systemctl status docker | grep running)" ]; then
+if [ "$(systemctl status docker | grep running)" ]; then
     docker stop jms_koko jms_guacamole
     docker rm jms_koko jms_guacamole
     docker rmi wojiushixiaobai/jms_koko:$Version wojiushixiaobai/jms_guacamole:$Version
     systemctl stop docker
 fi
 
-if [ ! "$(systemctl status jms_core | grep running)" ]; then
+if [ "$(systemctl status jms_core | grep running)" ]; then
     systemctl stop jms_core
 fi
 rm -rf /usr/lib/systemd/system/jms_core.service
@@ -28,28 +28,28 @@ rm -rf $install_dir/luna
 rm -rf $install_dir/jumpserver
 
 if [ $REDIS_HOST == 127.0.0.1 ]; then
-    if [ ! "$(systemctl status redis | grep running)" ]; then
+    if [ "$(systemctl status redis | grep running)" ]; then
         redis-cli flushall
         systemctl stop redis
     fi
 fi
 if [ $DB_HOST == 127.0.0.1 ]; then
-    if [ ! "$(systemctl status mariadb | grep running)" ]; then
+    if [ "$(systemctl status mariadb | grep running)" ]; then
         mysql -uroot -e"drop user '$DB_USER'@'$DB_HOST';drop database $DB_NAME;flush privileges;"
         systemctl stop mariadb
     fi
 fi
 
 if [ "$(systemctl status firewalld | grep running)" ]; then
-    if [ ! "$(firewall-cmd --list-all | grep $http_port)" ]; then
+    if [ "$(firewall-cmd --list-all | grep $http_port)" ]; then
         firewall-cmd --zone=public --remove-port=$http_port/tcp --permanent
         firewall-cmd --reload
     fi
-    if [ ! "$(firewall-cmd --list-all | grep $ssh_port)" ]; then
+    if [ "$(firewall-cmd --list-all | grep $ssh_port)" ]; then
         firewall-cmd --zone=public --remove-port=$ssh_port/tcp --permanent
         firewall-cmd --reload
     fi
-    if [ ! "$(firewall-cmd --list-all | grep 8080)" ]; then
+    if [ "$(firewall-cmd --list-all | grep 8080)" ]; then
         firewall-cmd --permanent --remove-rich-rule="rule family="ipv4" source address="172.17.0.0/16" port protocol="tcp" port="8080" accept"
         firewall-cmd --reload
     fi
